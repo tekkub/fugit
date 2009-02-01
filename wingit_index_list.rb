3ff5e2c613858e776d528dbf3e06936acbd68dfd
Tekkub <tekkub@gmail.com>
2009-01-31 23:41:03 -0700

    Add doubleclick handlers to inxes lists

--- wingit_index_list.rb
+++ wingit_index_list.rb
@@ -13,6 +13,26 @@ class WingitIndexList < Panel
 		box.add(@unstaged, 1, EXPAND)
 		box.add(@staged, 1, EXPAND)
 		self.set_sizer(box)
+
+		evt_listbox_dclick(@unstaged.get_id, :on_unstaged_double_click)
+		evt_listbox_dclick(@staged.get_id, :on_staged_double_click)
+
+		file_list = Dir.glob("**/*")
+		file_list.each {|file| @unstaged.append(file)}
+	end
+
+	def on_unstaged_double_click(event)
+		i = event.get_index
+		file = @unstaged.get_string(i)
+		@staged.append(file)
+		@unstaged.delete(i)
+	end
+
+	def on_staged_double_click(event)
+		i = event.get_index
+		file = @staged.get_string(i)
+		@unstaged.append(file)
+		@staged.delete(i)
 	end
 
 end
