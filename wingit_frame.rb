3c67b33d0616d875ddbd48e22a16ccc5d35dd54f
Tekkub <tekkub@gmail.com>
2009-02-04 02:42:22 -0700

    include Wx in base frame, clean out "Wx::" calls

--- wingit_frame.rb
+++ wingit_frame.rb
@@ -1,6 +1,6 @@
-#~ include Wx
+include Wx
 
-class WingitFrame < Wx::Frame
+class WingitFrame < Frame
 	attr_accessor :app_verion, :index, :commit, :diff
 
 	def initialize(title, version)
@@ -9,37 +9,37 @@ class WingitFrame < Wx::Frame
 		self.app_verion = version
 
 		# Set up our AUI manager
-		@mgr = Wx::AuiManager.new
+		@mgr = AuiManager.new
 		@mgr.set_managed_window(self)
 
-		set_min_size(Wx::Size.new(400,300))
+		set_min_size(Size.new(400,300))
 		setup_panes
 
 		icon_file = File.join(File.dirname(__FILE__), "icons", "plus_minus.gif")
-		self.icon = Wx::Icon.new(icon_file, Wx::BITMAP_TYPE_GIF)
+		self.icon = Icon.new(icon_file, BITMAP_TYPE_GIF)
 
-		menu_bar = Wx::MenuBar.new
+		menu_bar = MenuBar.new
 
 		# The "file" menu
-		menu_file = Wx::Menu.new
-		# Using Wx::ID_EXIT standard id means the menu item will be given the right label for the platform and language, and placed in the correct platform-specific menu - eg on OS X, in the Application's menu
-		menu_file.append(Wx::ID_SAVE, "&Save commit\tCtrl-S", "Save commit")
-		refresh = menu_file.append(Wx::ID_ANY, "&Refresh\tF5", "Refresh the index list")
-		menu_file.append(Wx::ID_EXIT, "E&xit\tAlt-X", "Quit this program")
+		menu_file = Menu.new
+		# Using ID_EXIT standard id means the menu item will be given the right label for the platform and language, and placed in the correct platform-specific menu - eg on OS X, in the Application's menu
+		menu_file.append(ID_SAVE, "&Save commit\tCtrl-S", "Save commit")
+		refresh = menu_file.append(ID_ANY, "&Refresh\tF5", "Refresh the index list")
+		menu_file.append(ID_EXIT, "E&xit\tAlt-X", "Quit this program")
 		menu_bar.append(menu_file, "&File")
 
 		# The "help" menu
-		menu_help = Wx::Menu.new
-		menu_help.append(Wx::ID_ABOUT, "&About...\tF1", "Show about dialog")
+		menu_help = Menu.new
+		menu_help.append(ID_ABOUT, "&About...\tF1", "Show about dialog")
 		menu_bar.append(menu_help, "&Help")
 
 		# Assign the menubar to this frame
 		self.menu_bar = menu_bar
 
-		evt_menu(Wx::ID_SAVE) {|event| @commit.on_commit_clicked}
+		evt_menu(ID_SAVE) {|event| @commit.on_commit_clicked}
 		evt_menu(refresh) {|event| update}
-		evt_menu(Wx::ID_EXIT, :on_quit)
-		evt_menu(Wx::ID_ABOUT, :on_about)
+		evt_menu(ID_EXIT, :on_quit)
+		evt_menu(ID_ABOUT, :on_about)
 
 		@mgr.update
 	end
@@ -50,25 +50,25 @@ class WingitFrame < Wx::Frame
 	#####################
 
 	def setup_panes
-		pi = Wx::AuiPaneInfo.new
-		size = Wx::Size.new(800, 150)
+		pi = AuiPaneInfo.new
+		size = Size.new(800, 150)
 		pi.bottom.set_name('commit').set_caption("Commit").set_layer(1).set_position(1).set_best_size(size).set_min_size(size).set_floating_size(size)
 		@commit = WingitCommit.new(self)
 		@mgr.add_pane(@commit, pi)
 
-		#~ pi = Wx::AuiPaneInfo.new
-		#~ size = Wx::Size.new(800, 75)
+		#~ pi = AuiPaneInfo.new
+		#~ size = Size.new(800, 75)
 		#~ pi.bottom.set_name('console').set_caption("Console").set_layer(3).set_position(1).set_best_size(size).set_min_size(size).set_floating_size(size).set_maximize_button.set_minimize_button
 		#~ @mgr.add_pane(WingitConsole.new(self), pi)
 
-		pi = Wx::AuiPaneInfo.new
-		size = Wx::Size.new(150, 350)
+		pi = AuiPaneInfo.new
+		size = Size.new(150, 350)
 		pi.left.set_name('index').set_caption("Index").set_layer(2).set_position(1).set_best_size(size).set_min_size(size).set_floating_size(size)
 		@index = WingitIndexList.new(self)
 		@mgr.add_pane(@index, pi)
 
-		pi = Wx::AuiPaneInfo.new
-		size = Wx::Size.new(200, 350)
+		pi = AuiPaneInfo.new
+		size = Size.new(200, 350)
 		pi.center_pane.set_name('diff')
 		@diff = WingitDiff.new(self)
 		@mgr.add_pane(@diff, pi)
